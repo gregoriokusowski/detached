bceb495fbdc9eadd9c3516be97af94a1a517454d
Gregório Kusowski <gregorio.kusowski@gmail.com>
2018-01-01 18:38:42 +0100

    Try it in a different way

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/gregoriokusowski/detached/cmd"
+import "fmt"
+
+var longDesc = `detached is a tool to create, manage and use remote development environments.
+
+Usage:
+
+        detached command [arguments]
+
+The commands are:
+
+        bootstrap   initialize your configuration
+        status      check your current configuration and remote setup
+        attach      attach a session to your remote machine, creating it if needed
+
+Use "detached help [command]" for more information about a command.`
 
 func main() {
-	cmd.Execute()
+	fmt.Println(longDesc)
 }
